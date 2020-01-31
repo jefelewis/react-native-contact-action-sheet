7ea220466c751e2750caa9746adedf83779d74a3
Jeff Lewis <jeffylew77@gmail.com>
2020-01-30 18:13:19 -0800

    [Dev] Adding test contacts

--- App.tsx
+++ App.tsx
@@ -9,9 +9,33 @@ import ContactActionSheet from './src/ContactActionSheet';
 // React Native App
 const App = () => {
 
+  // Contacts
+  const contacts = [
+    {
+      title: 'Building Department',
+      type: 'Phone Number',
+      contact: '(555) 555-5555',
+    },
+    {
+      title: 'City Clerk',
+      type: 'Phone Number',
+      contact: '(777) 777-7777',
+    },
+    {
+      title: 'Building Department',
+      type: 'Email',
+      contact: 'building@city.com',
+    },
+    {
+      title: 'City Clerk',
+      type: 'Email',
+      contact: 'cityclerk@city.com',
+    },
+  ];
+
   return (
-    <SafeAreaView style={{ display: 'flex', flex: 1 }}>
-      <ContactActionSheet />
+    <SafeAreaView>
+      <ContactActionSheet contactsList={contacts} />
     </SafeAreaView>
   )
 };
