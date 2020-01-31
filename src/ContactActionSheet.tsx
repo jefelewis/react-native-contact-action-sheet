378421f7dc9c1a66436a25278c196ff6645b2deb
Jeff Lewis <jeffylew77@gmail.com>
2020-01-30 17:29:32 -0800

    [Dev] Cancel Button and Contact Selector working + Adding styling

--- src/ContactActionSheet.tsx
+++ src/ContactActionSheet.tsx
@@ -1,6 +1,7 @@
 // Imports: Dependencies
 import React, { useState } from 'react';
-import { Button, Dimensions, Platform, StyleSheet, Text, View, Linking, TouchableOpacity } from 'react-native';
+import { ActionSheetIOS, Button, Dimensions, Platform, StyleSheet, Text, View, Linking, TouchableOpacity, Alert } from 'react-native';
+import Modal from 'react-native-modal';
 import { ActionSheetCustom } from 'react-native-custom-actionsheet';
 import Icon from 'react-native-vector-icons/Ionicons';
 Icon.loadFont();
@@ -9,6 +10,19 @@ Icon.loadFont();
 const { height, width } = Dimensions.get('window');
 
 // TypeScript: Types
+interface Contact {
+  title: string;
+  type: 'Email' | 'Phone Number';
+  contact: string;
+}
+
+interface Props {
+  contactsList: Array<Contact>,
+}
+
+
+
+
 // interface Props {
 //   title?: string;
 //   mode: 'calendar' | 'spinner' | 'default';
@@ -16,15 +30,29 @@ const { height, width } = Dimensions.get('window');
 // }
 
 // Component: Contact Action Sheet
-const ContactActionSheet = (props) => {
+const ContactActionSheet = (props: Props) => {
   // React Hooks: State
+  const [ modalVisible, toggle ] = useState(false);
   const [ selected, selectContact ] = useState(false);
-
-  let actionSheet: any = null;
 
   // React Hooks: Lifecycle Methods
 
-  const showActionSheet = () => {
+  // Toggle Modal
+  const toggleModal = () => {
+    try {
+      // Check Platform (iOS)
+      if (Platform.OS === 'ios') {
+        // React Hook: Toggle Modal
+        toggle((modalVisible: boolean) => !modalVisible);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+  
+  let actionSheet: any = null;
+  const showCustomActionSheet = () => {
     actionSheet.show();
   };
  
@@ -33,6 +61,22 @@ const ContactActionSheet = (props) => {
   };
  
   const handlePress = (index: any) => selectContact(index);
+
+
+  // Native Action Sheet
+  const showActionSheet = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['Cancel', 'Call (Building Department)', 'Call (City Clerk)', 'Email (Building Department)', 'Email (City Clerk)'],
+        cancelButtonIndex: 0,
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 1) {
+          /* destructive action */
+        }
+      },
+    );
+  };
 
   // Press Call
   const pressCall = (phoneNumber: string) => {
@@ -49,55 +93,92 @@ const ContactActionSheet = (props) => {
   const DESTRUCTIVE_INDEX = 4
   const options = [
     'Cancel',
-    {
-      component: <View style={{ display: 'flex', flexDirection: 'row', width: width - 32, alignItems: 'center' }}>
-          <Icon name="ios-call" size={28} style={{ marginLeft: 16, marginRight: 24 }} color="#323232"></Icon>
-          <View>
-            <Text style={{ fontFamily: 'System', fontSize: 17, fontWeight: '500', marginBottom: 4, color: '#323232' }}>Building Department</Text>
-            <Text style={{ fontFamily: 'System', fontSize: 15, fontWeight: '400', color: '#7D7D7D' }}>(555) 555-5555</Text>
-          </View>
-        </View>,
-      height: 70,
-    },
-    {
-      component: <View style={{ display: 'flex', flexDirection: 'row', width: width - 32, alignItems: 'center' }}>
-          <Icon name="ios-call" size={28} style={{ marginLeft: 16, marginRight: 24 }} color="#323232"></Icon>
-          <View>
-            <Text style={{ fontFamily: 'System', fontSize: 17, fontWeight: '500', marginBottom: 4, color: '#323232' }}>City Clerk</Text>
-            <Text style={{ fontFamily: 'System', fontSize: 15, fontWeight: '400', color: '#7D7D7D' }}>(777) 777-7777</Text>
-          </View>
-        </View>,
-      height: 70,
-    },
-    {
-      component: <View style={{ display: 'flex', flexDirection: 'row', width: width - 32, alignItems: 'center' }}>
-          <Icon name="ios-mail" size={28} style={{ marginLeft: 16, marginRight: 24 }} color="#323232"></Icon>
-          <View>
-            <Text style={{ fontFamily: 'System', fontSize: 17, fontWeight: '500', marginBottom: 4, color: '#323232' }}>Building Department</Text>
-            <Text style={{ fontFamily: 'System', fontSize: 15, fontWeight: '400', color: '#7D7D7D' }}>building@city.com</Text>
-          </View>
-        </View>,
-      height: 70,
-    },
-    {
-      component: <View style={{ display: 'flex', flexDirection: 'row', width: width - 32, alignItems: 'center' }}>
-          <Icon name="ios-mail" size={28} style={{ marginLeft: 16, marginRight: 24 }} color="#323232"></Icon>
-          <View>
-            <Text style={{ fontFamily: 'System', fontSize: 17, fontWeight: '500', marginBottom: 4, color: '#323232' }}>City Clerk</Text>
-            <Text style={{ fontFamily: 'System', fontSize: 15, fontWeight: '400', color: '#7D7D7D' }}>cityclerk@city.com</Text>
-          </View>
-        </View>,
-      height: 70,
-    },
+    'Test #1',
+    'Test #2',
+    'Test #3',
+    // {
+    //   component: <View style={{ display: 'flex', flexDirection: 'row', width: width - 32, alignItems: 'center' }}>
+    //       <Icon name="ios-call" size={28} style={{ marginLeft: 16, marginRight: 24 }} color="#323232"></Icon>
+    //       <View>
+    //         <Text style={{ fontFamily: 'System', fontSize: 17, fontWeight: '500', marginBottom: 4, color: '#323232' }}>Building Department</Text>
+    //         <Text style={{ fontFamily: 'System', fontSize: 15, fontWeight: '400', color: '#7D7D7D' }}>(555) 555-5555</Text>
+    //       </View>
+    //     </View>,
+    //   height: 70,
+    // },
+    // {
+    //   component: <View style={{ display: 'flex', flexDirection: 'row', width: width - 32, alignItems: 'center' }}>
+    //       <Icon name="ios-call" size={28} style={{ marginLeft: 16, marginRight: 24 }} color="#323232"></Icon>
+    //       <View>
+    //         <Text style={{ fontFamily: 'System', fontSize: 17, fontWeight: '500', marginBottom: 4, color: '#323232' }}>City Clerk</Text>
+    //         <Text style={{ fontFamily: 'System', fontSize: 15, fontWeight: '400', color: '#7D7D7D' }}>(777) 777-7777</Text>
+    //       </View>
+    //     </View>,
+    //   height: 70,
+    // },
+    // {
+    //   component: <View style={{ display: 'flex', flexDirection: 'row', width: width - 32, alignItems: 'center' }}>
+    //       <Icon name="ios-mail" size={28} style={{ marginLeft: 16, marginRight: 24 }} color="#323232"></Icon>
+    //       <View>
+    //         <Text style={{ fontFamily: 'System', fontSize: 17, fontWeight: '500', marginBottom: 4, color: '#323232' }}>Building Department</Text>
+    //         <Text style={{ fontFamily: 'System', fontSize: 15, fontWeight: '400', color: '#7D7D7D' }}>building@city.com</Text>
+    //       </View>
+    //     </View>,
+    //   height: 70,
+    // },
+    // {
+    //   component: <View style={{ display: 'flex', flexDirection: 'row', width: width - 32, alignItems: 'center' }}>
+    //       <Icon name="ios-mail" size={28} style={{ marginLeft: 16, marginRight: 24 }} color="#323232"></Icon>
+    //       <View>
+    //         <Text style={{ fontFamily: 'System', fontSize: 17, fontWeight: '500', marginBottom: 4, color: '#323232' }}>City Clerk</Text>
+    //         <Text style={{ fontFamily: 'System', fontSize: 15, fontWeight: '400', color: '#7D7D7D' }}>cityclerk@city.com</Text>
+    //       </View>
+    //     </View>,
+    //   height: 70,
+    // },
   ];
+
+
+  // Render Contact Selectors
+  const renderContactSelectors = (props: Props) => {
+    try {
+      if (props.contactsList.length >= 6) {
+        console.warn('Error: Maximum of 6 contacts allowed.');
+      }
+      else {
+        return props.contactsList.map((contact: Contact) => {
+          // IF/ELSE FOR EMAIL/PHONE NUMBER
+          // return (
+            <View style={styles.contactSelector}>
+              <Text>{contact.title}</Text>
+              <Text>{contact.contact}</Text>
+            </View>
+          // );
+        });
+      }
+    }
+    catch (error) {
+      console.log(error)
+    }
+  };
 
   return (
     <View style={styles.container}>
       {/* <Text>Hi</Text> */}
 
       <Button
-        title="Show Action Sheet"
+        title="Show Native Action Sheet"
         onPress={showActionSheet}
+      />
+
+      <Button
+        title="Show Custom Action Sheet"
+        onPress={showCustomActionSheet}
+      />
+
+      <Button
+        title="Show Modal"
+        onPress={() => toggleModal()}
       />
 
       <ActionSheetCustom
@@ -108,7 +189,32 @@ const ContactActionSheet = (props) => {
         cancelButtonIndex={CANCEL_INDEX}
         destructiveButtonIndex={DESTRUCTIVE_INDEX}
         onPress={handlePress}
+        style={styles.actionSheetContainer}
       />
+
+      <Modal
+        isVisible={modalVisible}
+        style={styles.modal}
+        backdropOpacity={.30}
+        onBackdropPress={() => alert('Fuck')}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.contactListContainer}>
+            {/* {renderContactSelectors(props)} */}
+            <TouchableOpacity style={styles.contactSelector}>
+              <Icon name="ios-call" size={28} style={styles.icon} color="#323232"></Icon>
+              <View>
+                <Text style={styles.contactTitle}>{props.contactsList[0].title}</Text>
+                <Text style={styles.emailPhone}>{props.contactsList[0].contact}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity onPress={() => toggleModal()} style={styles.cancelButtonContainer}>
+            <Text style={styles.cancelText}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -122,9 +228,65 @@ const styles = StyleSheet.create({
     // marginRight: 16,
     // justifyContent: 'center',
   },
-  contactContainer: {
+  modal: {
+    margin: 0,
+  },
+  modalContainer: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  contactListContainer: {
+    width: width - 20,
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  contactSelector: {
+    width: width - 20,
+    height: 65,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 12,
     display: 'flex',
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginLeft: 16,
+    marginRight: 24,
+  },
+  contactTitle: {
+    fontFamily: 'System',
+    fontSize: 17,
+    fontWeight: '500',
+    marginBottom: 4,
+    color: '#323232',
+  },
+  emailPhone: {
+    fontFamily: 'System',
+    fontSize: 15,
+    fontWeight: '400',
+    color: '#7D7D7D',
+  },
+  cancelButtonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width - 20,
+    height: 60,
+    backgroundColor: '#FFFFFF',
+    marginBottom: 35,
+    borderRadius: 12,
+  },
+  cancelText: {
+    fontFamily: 'System',
+    fontSize: 20,
+    color: '#007AFF',
+    fontWeight: '600',
+  },
+  actionSheetContainer: {
+    // paddingBottom: 40
+    borderWidth: 2,
+    borderColor: 'green',
+    backgroundColor: 'red',
   },
 });
 
