16544a4863d1e6fd70de6cca750fb3564654a65e
Jeff Lewis <jeffylew77@gmail.com>
2021-05-16 18:21:28 -0700

    [Dev] Exporting ContactItem type

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // Imports: Components
 import ContactActionSheet from './components/ContactActionSheet';
 
+// Imports: TypeScript Types
+import { ContactItem } from './types/types';
+
 // Exports
 export {
   ContactActionSheet,
+  ContactItem,
 };
